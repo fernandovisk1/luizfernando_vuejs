dc613bce98ffbbd2b8039b641e62c4f8b5a145f0
unknown <fernandoplaying@hotmail.com>
2020-08-24 21:30:15 -0300

    commit5

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -1,55 +1,32 @@
-import { register } from 'register-service-worker';
-import AlertEvent from './events/AlertEvent';
+/* eslint-disable no-console */
+
+import { register } from 'register-service-worker'
 
 if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
-    ready() {
+    ready () {
       console.log(
-        'App is being served from cache by a service worker.\n'
-        + 'For more details, visit https://goo.gl/AFskqB',
-      );
+        'App is being served from cache by a service worker.\n' +
+        'For more details, visit https://goo.gl/AFskqB'
+      )
     },
-    registered() {
-      console.log('Service worker has been registered.');
+    registered () {
+      console.log('Service worker has been registered.')
     },
-    cached() {
-      console.log('Content has been cached for offline use.');
+    cached () {
+      console.log('Content has been cached for offline use.')
     },
-    updatefound() {
-      console.log('New content is downloading.');
+    updatefound () {
+      console.log('New content is downloading.')
     },
-    updated(registration) {
-      console.log('New content is available; please refresh.');
-      const { waiting } = registration;
-      if (!waiting) return;
-      document.dispatchEvent(new AlertEvent({
-        message: 'Uma nova versão está disponivel',
-        button: {
-          text: 'Atualizar',
-          handler() {
-            waiting.postMessage({
-              type: 'SKIP_WAITING',
-            });
-          },
-        },
-      }));
+    updated () {
+      console.log('New content is available; please refresh.')
     },
-    offline() {
-      console.log('No internet connection found. App is running in offline mode.');
+    offline () {
+      console.log('No internet connection found. App is running in offline mode.')
     },
-    error(error) {
-      console.error('Error during service worker registration:', error);
-    },
-  });
-
-  let refreshing = false;
-  navigator.serviceWorker.addEventListener(
-    'controllerchange',
-    () => {
-      if (refreshing) return;
-
-      refreshing = true;
-      window.location.reload();
-    },
-  );
+    error (error) {
+      console.error('Error during service worker registration:', error)
+    }
+  })
 }
